c1c245722ec0ea828673cf665f5fa8321920d9c4
Cyrus-M2 <devsben345@gmail.com>
2025-11-10 13:15:11 +0300

    Created file for python control structures - called codeblocks.py

---
+++ codeblocks.py
@@ -0,0 +1,10 @@
+# if - true, elif - check for other condition if the if condition is not satisfied
+# elif
+
+# age = 20
+# if age > 18:
+#     print("This person is a young adult")
+# elif age == 18:
+#     print("This person's age is at 18")    
+# else:
+#     print("The age comparison does not meet conditions")    
